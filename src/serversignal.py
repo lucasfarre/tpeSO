bf77f11fdc5fc030cc4943721bb5db676b50d966
Franco Román Meola <franco.meola@icloud.com>
2014-04-19 19:47:02 -0300

    Ahora serversignal.py usa dbback.py. Cambios menores.

--- src/serversignal.py
+++ src/serversignal.py
@@ -1,9 +1,13 @@
+# -'- coding: iso8859-1 -'-
+
 from cfunctions import *
 from functions import *
-from serverback import *
+from dbback import *
 import os
 
-##### SRV Signal Server v1.5 #####
+####################################################################################################
+##### SRV Signal Server v2.0
+####################################################################################################
 
 transitionFileName = '.pet.srv'
 
@@ -23,44 +27,30 @@ def getClientPID(petition):
 ##### Main
 ####################################################################################################
 
-print('Server: Files & Signals')
+#TODO Falta poner un lock bloqueante porque sino se pisan los checkins!!!.
+#HAY QUE BLOQUEAR LA BASE DE DATOS CADA VEZ QUE UN CLIENTE LA ESTÁ USANDO. ES LO MÁS FACIL
+#SINO HAY QUE CONTEMPLAR MUCHOS CASOS. CON EL LOCK BLOQUEANTE EL CLIENTE SE QUEDA ESPERANDO
+#AUTOMATICAMENTE HASTA QUE EL ARCHIVO SE DESBLOQUEE!!!
 
+#TODO Acá el servidor no catchea el Ctrl-C porque se queda colgado en C hasta que recibe alguna señal
+#¿como arreglamos eso?
+
+print('Server: Files & Signals')
 while True:
     serverInit()
+    print('Esperando recibir una señal')
     recieveSignal()
     fd = open(transitionFileName,'r+')
     petition = fd.read()
     petition = fromJson(petition)
     clientPID = getClientPID(petition)
-    fd.seek(0)
-    fd.truncate()
+    print('Conexión establecida con el cliente PID' + str(clientPID))
     if petition['id'] == 1:
-        #getAllFlights
-        data = getAllFlights()
-        fd.write(toJson(data))
-        fd.flush()
-        os.fsync(fd.fileno())
+        reWrite(fd,toJson(getAllFlights()))
         fd.close()
-        sendSignal(clientPID)
-        recieveSignal()
-    if petition['id'] == 2:
-        #checkIn
-        checkIn(petition['data'],petition['passenger'],petition['seat'])
-        fd.flush()
-        os.fsync(fd.fileno())
-        fd.close()
-        sendSignal(clientPID)
-    if petition['id'] == 3:
-        #addAFlight
-        addFlight(petition['data'])
-        fd.flush()
-        os.fsync(fd.fileno())
-        fd.close()
-        sendSignal(clientPID)
-    if petition['id'] == 4:
-        #removeAFlight
-        removeFlight(petition['data'])
-        fd.flush()
-        os.fsync(fd.fileno())
-        fd.close()
-        sendSignal(clientPID)
+    if petition['id'] == 2: checkIn(petition['data'],petition['passenger'],petition['seat'])
+    if petition['id'] == 3: addFlight(petition['data'])
+    if petition['id'] == 4: removeFlight(petition['data'])
+    sendSignal(clientPID)
+    if petition['id'] == 1: recieveSignal()
+    print('Conexión finalizada con el cliente PID' + str(clientPID))
