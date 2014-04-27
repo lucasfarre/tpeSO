98accc3cf2e3820fb0a853c2c09c0f5fbf8db5eb
Franco Román Meola <franco.meola@icloud.com>
2014-04-27 11:53:03 -0300

    Implementación Parcial de MQPosix.

--- src/mqposixclient.py
+++ src/mqposixclient.py
@@ -1,16 +1,17 @@
-# from abc import ABCMeta
-import dbback
+# -'- coding: iso8859-1 -'-
+
 import classes
 import cfunctions
-import socket
 import functions
-import time
+import os
+from clientFront import *
+
+####################################################################################################
+##### SRV MQ Posix Client v2.0
+##### Ejercicio 2.a.4
+####################################################################################################
 
 class Client():
-#     __metaclass__ = ABCMeta
-    
-    def __init__(self):
-        pass
     
     def open(self):
         self.qin = '/qout'
@@ -20,10 +21,9 @@ class Client():
         self.open()
         id = int(id)
         header = classes.package(str(id).zfill(4), '0000060', None)
-        header = functions.toJson(header)
+        header = functions.toPrettyJson(header)
         cfunctions.mqposixSend(header, self.qout)
         json = 0
-        print 'Entro al while'
         e = True
         json = ''
         while e:
@@ -35,30 +35,38 @@ class Client():
                 pass
             else:
                 e = False
-        print functions.toJson(response)
-#         if len(json) >= 60:
-#             json = json[:60]
-#             header = functions.fromJson(json)
-#             length = int(header['length'])
-#             cfunctions.down(self.semid, 0)
-#             json = cfunctions.memread(self.mem)
-#             cfunctions.up(self.semid, 0)
-#             if len(json) >= length:
-#                 json = json[:length]
-#                 print 'Response: \n' + json
-#                 response = functions.fromJson(json)
-#         self.close()     
-        
+        return functions.toJson(response)
+    
+    def MQPosixGetAllFlights(self):
+        return functions.fromJson(self.request(1))['data']
+    
+    def reserveAFlight(self,flights):
+        selection = reserveASeat(flights)
+        if selection != -1:
+            self.MPPosixCheckIn(selection['flightId'], selection['passenger'], selection['seat'])
+    
+    def MPPosixCheckIn(self,flightId, passenger, seat):
+        petition = newPetitionMsg(2,str(os.getpid()),flightId,passenger,seat)
+        petition = functions.toJson(petition)
+        cfunctions.mqposixSend(petition, self.qout)
+    
 def main():
-#     ip = raw_input('Ingrese el IP del servidor: ')
-#     port = raw_input('Ingrese el puerto del servidor: ')
-    s = Client()
-    id = raw_input('Ingrese el ID de la peticion: ')
-    s.request(id)          
+    s = Client()   
+    s.open()
+    print('Cliente: MQ Posix')
+    print("S.R.V. Sistema de Reserva de Vuelos\n")
+    option = 0;
+    while option != '6':
+        mainMenu()
+        option = raw_input('Ingrese una Opción: ')
+        print('')
+        flights = s.MQPosixGetAllFlights()
+        if option == '1': checkAFlight(flights)
+        if option == '2': s.reserveAFlight(flights)
+        if option == '3': s.addAFlight()
+        if option == '4': s.removeAFlight(flights)
+        if option == '5': aboutUs()
+        if option == '6': quitClient()    
         
 if __name__ == "__main__":
     main()
-        
-        
-        
-        
--- src/mqposixserver.py
+++ src/mqposixserver.py
@@ -1,15 +1,16 @@
-# from abc import ABCMeta
-import dbback
+from dbback import *
 import classes
 import cfunctions
-import socket
 import functions
 import os
-import time
 import string
 
+####################################################################################################
+##### SRV MQ Posix Server v2.0
+##### Ejercicio 2.a.4
+####################################################################################################
+
 class Server:
-#     __metaclass__ = ABCMeta
 
     def open(self):
         self.qin = '/qin'
@@ -20,64 +21,29 @@ class Server:
         up = 1
         while up == 1:
             open = True
-            
             json = 0
             while not isinstance(json, str):
                 json = cfunctions.mqposixReceive(self.qin)
-            
-            if len(json) >= 60:
-                json = json[:60]
-                print 'Request received: \n' + json
-                request = functions.fromJson(json)
-                id = int(request['id'])
-                if id == 0:
-                    open = False
-                    r = None
-                if id == 1:
-                    response = classes.package('0001', '0000000', dbback.getAllFlights())
-                    response = functions.toJson(response)
-                    length = str(len(response)).zfill(7)
-                    response = classes.package('0001', '0000000', dbback.getAllFlights())
-                    response = functions.toJson(response)
-                    
-                    header = classes.package('0001', length, None)
-                    header = functions.toJson(header)
+            print 'Peticion Recibida: \n' + json
+            request = functions.fromJson(json)
+            id = int(request['id'])
+            if id == 1:
+                response = classes.package('0001', '0000000', getAllFlights())
+                response = functions.toPrettyJson(response)
+                l = string.split(response)
+                for s in l:
+                    print s
+                    errno = cfunctions.mqposixSend(s, self.qout)
+                    if errno != 0:
+                        os.strerror(errno)       
+                print 'mande'
+            if id == 2:
+                checkIn(request['data'],request['passenger'],request['seat'])
 
-                    
-#                     if cfunctions.memwrite(self.mem, header) != -1:
-#                         print 'Header sent: \n' + header
-#                     step = 1000
-#                     for i in range(0, len(response), 1000):
-#                         slice = response[i:step]
-#                         print slice
-#                         step += 1000
-#                         errno = cfunctions.mqsvSend(slice, self.qout)
-# #                         time.sleep(1)
-#                         if errno != 0:
-#                             os.strerror(errno)
-                    l = string.split(response)
-                    for s in l:
-                        print s
-                        errno = cfunctions.mqposixSend(s, self.qout)
-                        if errno != 0:
-                            os.strerror(errno)       
-                    print 'mande'
-#                     print 'enviando...'
-#                     print os.strerror(cfunctions.mqsvSend(response, self.qout))
-#                     print 'envie response'
-
-                if id == 2:
-                    pass   
-#                 self.disconnect()
-#         self.close()
         
 def main():
     s = Server()
     s.run()        
         
 if __name__ == "__main__":
-    main()
-        
-        
-        
-        
+    main()  
